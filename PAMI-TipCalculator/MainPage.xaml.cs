1650fbe1bec6d27e8de76fffdbf22dd2c3f580e1
Jonnathan2020 <jonnathan.santos15@gmail.com>
2024-08-23 22:43:22 -0300

    Adiciona logica e ações nos botões

--- PAMI-TipCalculator/MainPage.xaml.cs
+++ PAMI-TipCalculator/MainPage.xaml.cs
@@ -11,12 +11,14 @@
 
         private void SetLowTip_Clicked(object sender, EventArgs e)
         {
-            Slider.ValueProperty = 15;
+            TipSlider.Value = 15;
+            TipPercentLabel.Text = TipSlider.Value.ToString() + "%";
         }
 
         private void SetHighTip_Clicked(object sender, EventArgs e)
         {
-
+            TipSlider.Value = 20;
+            TipPercentLabel.Text = TipSlider.Value.ToString() + "%";
         }
 
         private void RoundUpButton_Clicked(object sender, EventArgs e)
@@ -27,6 +29,21 @@
         private void RoundDownButton_Clicked(object sender, EventArgs e)
         {
 
+        }
+
+        private void TipSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            TipPercentLabel.Text = TipSlider.Value.ToString("#.##") + "%";
+        }
+
+        private void AmountEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            double Valor = Convert.ToDouble(AmountEntry.Text);
+            double ValorDaGorjeta = Valor * (TipSlider.Value / 100);
+            double ValorTotal = ValorDaGorjeta + Valor;
+
+            TotalLabel.Text = ValorTotal.ToString();
+            TipLabel.Text = ValorDaGorjeta.ToString();
         }
     }
 
